0f78080c112c946b9f9c19a0d8aecf3baa64c7cb
Dávid Lévai <david@davidlevai.com>
2019-03-13 14:00:28 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = () => ({})
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/{reponame}' : '',
+};
