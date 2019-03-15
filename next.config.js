b54bb340b8309ae6623078e6375bd6462c620733
Dávid Lévai <david@davidlevai.com>
2019-03-15 14:31:14 +0100

    nowsh update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-const withCSS = require('@zeit/next-css');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : require('next-server/constants');
 
-module.exports = withCSS({});
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+    };
+  }
+  const withCSS = require('@zeit/next-css');
+  return withCSS({});
+};
